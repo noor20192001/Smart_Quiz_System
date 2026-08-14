250c93d531345795bb523530928aff5c32fa3647
noor20192001 <57191527+noor20192001@users.noreply.github.com>
2026-08-14 17:36:39 +0300

    Update question_screen.py

--- question_screen.py
+++ question_screen.py
@@ -22,35 +22,35 @@ def load_qustion(filename,default_list):
 default_easy = [{"id": "1.1.", "question": "What's Python?",
                      "options": ["A. Programming Language"," B. Datatbase"," C. Operating System"," D. Browser"],
                      "difficulty":"Easy", "answer":"A"},
-            {"id": "1.2.", "question": "What's the oop?",
-                     "options": ["A. Programming Language"," B. Datatbase"," C. Operating System"," D. Browser"],
-                     "difficulty":"Easy","answer":"A"},
-            {"id": "1.3.", "question": "What's the use of the set function?",
-                     "options": ["A. Programming Language"," B. Datatbase"," C. Operating System"," D. Browser"],
-                     "difficulty":"Easy","answer":"A"},]
+            {"id": "1.2.", "question": "What is the correct extension for Python files",
+                     "options": ["A. .pt", "B. .pyt", "C. .py", "D. .pyc"],
+                     "difficulty":"Easy","answer":"C"},
+            {"id": "1.3.", "question": "How do you start writing a comment in Python",
+                     "options": ["A. //", "B. #", "C. /*", "D. --"],
+                     "difficulty":"Easy","answer":"B"},]
 
 filename_easy = "easy.json"
 
-default_medium =[{"id": "2.1.", "question": "What's the use of the pop function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
+default_medium =[{"id": "2.1.", "question": "Which function is used to remove leading and trailing whitespace from a string?",
+             "options": ["A. strip()", "B. trim()"," C. cut()", "D. len()"],
              "difficulty": "Medium","answer":"A"},
-            {"id": "2.2.", "question": "What's the use of the pop function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
-             "difficulty": "Medium","answer":"A"},
-            {"id": "2.3.", "question": "What's the use of the pop function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
-             "difficulty": "Medium","answer":"A"},]
+            {"id": "2.2.", "question": " What is the result of the following code: len(['apple', 'banana','cherry'])? ",
+             "options": ["A. 1", "B. 2", "C. 3", "D. 15"],
+             "difficulty": "Medium","answer":"C"},
+            {"id": "2.3.", "question": "What is the output of the following code: print(10 // 3)?",
+             "options": ["A. 3.33", "B. 3", "C. 1", "D. 0"],
+             "difficulty": "Medium","answer":"B"},]
 
 filename_medium = "medium.json"
 
-default_hard =[{"id": "3.1.", "question": "What's the use of the set function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
-             "difficulty": "Hard","answer":"A"},
-            {"id": "3.2.", "question": "What's the use of the set function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
-             "difficulty": "Hard","answer":"A"},
-            {"id": "3.3.", "question": "What's the use of the set function?",
-             "options": ["A. Programming Language", "B. Datatbase", "C. Operating System", "D. Browser"],
+default_hard =[{"id": "3.1.", "question": "Which of the following data types is considered immutable? ",
+             "options":  ["A. List", "B. Dictionary", "C. Set"," D. Tuple"],
+             "difficulty": "Hard","answer":"D"},
+            {"id": "3.2.", "question": "What is the keyword used to create a function in Python?",
+             "options": ["A. define", "B. func"," C. def"," D. function"],
+             "difficulty": "Hard","answer":"C"},
+            {"id": "3.3.", "question": "What is the correct way to open a file for reading only in Python?",
+             "options": ["A. open(file, 'w')"," B. open(file, 'r')"," C. open(file, 'a')", "D. open(file, 'x')"],
              "difficulty": "Hard","answer":"A"},
             ]
 
